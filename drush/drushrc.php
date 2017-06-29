9847063f5d9cdb70f1d642cefc6388bd526643de
Moshe Weitzman <weitzman@tejasa.com>
2017-06-29 14:57:07 -0400

    Update saved Drush site aliases and clean Drush config (#901)
    * Update saved site aliases and clean up drush config
    
    Note that it is no longer necessary for each dev to get drush own drush site aliases. I’ve removed this step from the README.md
    
    * Change drush launcher to set -r option which will then get our config file read.
    
    * Wipe local DB before sql-sync

--- drush/drushrc.php
+++ drush/drushrc.php
@@ -1,9 +1,6 @@
 <?php
 
-$options['include'] = [
-  // @todo Not needed after https://github.com/previousnext/drush_cmi_tools/pull/8
-  "../vendor/previousnext/drush_cmi_tools",
-];
+
 /**
  * Using the flag "--structure-tables-key=common" on sql-dump and sql-sync will cause
  * the structure but not the data to be dumped for these tables.
@@ -14,7 +11,7 @@ $command_specific['sql-sync'] = [
   'structure-tables-key' => 'common',
 ];
 
-$command_specific['config-import-plus'] = [
-  'source' => '../conf/drupal/config',
-  'delete-list' => '../conf/drupal/delete.yml'
-];
+# @todo - 'root' can't be set in a drushrc. for now run drush comamnds from docroot.
+# this is fixed in drush9.
+// $options['root'] = __DIR__ . "/docroot";
+$options['uri'] = "http://mass.local";
