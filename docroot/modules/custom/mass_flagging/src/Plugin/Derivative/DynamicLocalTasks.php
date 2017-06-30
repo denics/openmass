a7aa6c43d16a2a170ba1acad65c60621c10521f6
Rob Powell <rob.p.powell@gmail.com>
2017-06-30 13:32:16 -0400

    WIP: OOP DI

--- docroot/modules/custom/mass_flagging/src/Plugin/Derivative/DynamicLocalTasks.php
+++ docroot/modules/custom/mass_flagging/src/Plugin/Derivative/DynamicLocalTasks.php
@@ -8,11 +8,47 @@
 namespace Drupal\mass_flagging\Plugin\Derivative;
 
 use Drupal\Component\Plugin\Derivative\DeriverBase;
-
+use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
 /**
  * Defines dynamic local tasks.
  */
-class DynamicLocalTasks extends DeriverBase {
+class DynamicLocalTasks extends DeriverBase implements ContainerDeriverInterface {
+
+  /**
+   * The base plugin ID.
+   *
+   * @var string
+   */
+  protected $basePluginId;
+
+  /**
+   * Routematch.
+   *
+   * @var Drupal\Core\Routing\RouteMatchInterface;
+   */
+  protected $routeMatch;
+
+  /**
+   * Constructs a \Drupal\mass_flagging\Plugin\Derivative\ViewsLocalTask instance.
+   *
+   * @param string $base_plugin_id
+   * @param Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The route match.
+   */
+  public function __construct($base_plugin_id, RouteMatchInterface $route_match) {
+    $this->basePluginId = $base_plugin_id;
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, $base_plugin_id) {
+    return new static(
+      $container->get('router.matcher')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -20,9 +56,21 @@ class DynamicLocalTasks extends DeriverBase {
   public function getDerivativeDefinitions($base_plugin_definition) {
     // Implement dynamic logic to provide values for the same keys as in example.links.task.yml.
     $this->derivatives['mass_flagging.task_id'] = $base_plugin_definition;
-    $this->derivatives['mass_flagging.task_id']['title'] = "Watching";
+    #TODO: get txt value from flag to replace static
+    $this->derivatives['mass_flagging.task_id']['title'] = "Watch";
+
+    $node = $this->routematch->getParameter('node');
+    if ($node) {
+      $node_id = $node->id();
+    }
 //    $this->derivatives['mass_flagging.task_id']['route_name'] = 'entity.node.delete_form';
     $this->derivatives['mass_flagging.task_id']['route_name'] = 'flag.action_link_flag';
+    $this->derivatives['mass_flagging.task_id']['route_parameters'] = [
+      'flag' => 'watch_content',
+      'entity_id' => $node_id
+    ];
+
+
     $this->derivatives['mass_flagging.task_id']['base_route'] = 'entity.node.canonical';
     return $this->derivatives;
   }
