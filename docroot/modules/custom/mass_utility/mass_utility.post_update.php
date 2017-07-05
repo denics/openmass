e8f89e27a51d543b1540a8ac9c8b115be8ebc9a6
Moshe Weitzman <weitzman@tejasa.com>
2017-07-05 15:49:28 -0400

    Coding standards.

--- docroot/modules/custom/mass_utility/mass_utility.post_update.php
+++ docroot/modules/custom/mass_utility/mass_utility.post_update.php
@@ -1,16 +1,21 @@
 <?php
+
 use Drupal\node\Entity\Node;
+
+/**
+ * @file
+ *   Run updates after updatedb has completed.
+ */
 
 /**
  * Populate new address field.
  *
- * An implementation of hook_post_update_NAME().
- * See https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Extension%21module.api.php/function/hook_post_update_NAME/8.2.x
+ * See https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Extension%21module.api.php/function/hook_post_update_NAME/8.2.x.
  */
 function mass_utility_post_update_address_field() {
   $eq = \Drupal::entityQuery('node');
   $nids = $eq->condition('type', 'contact_information')->execute();
-  /** @var Node[] $nodes */
+  /** @var Drupal\node\Entity\Node[] $nodes */
   $nodes = Node::loadMultiple($nids);
   foreach ($nodes as $node) {
     // @todo Parse local CSV file.
