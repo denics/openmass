3fecd6b25394de33b65acba1c59f07b6e8a58c7a
Moshe Weitzman <weitzman@tejasa.com>
2017-07-06 10:53:04 -0400

    Update link.

--- docroot/modules/custom/mass_utility/mass_utility.post_update.php
+++ docroot/modules/custom/mass_utility/mass_utility.post_update.php
@@ -10,7 +10,7 @@ use Drupal\node\Entity\Node;
 /**
  * Populate new address field.
  *
- * See https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Extension%21module.api.php/function/hook_post_update_NAME/8.2.x.
+ * See https://api.drupal.org/api/drupal/core%21lib%21Drupal%21Core%21Extension%21module.api.php/function/hook_post_update_NAME/8.3.x.
  */
 function mass_utility_post_update_address_field() {
   $eq = \Drupal::entityQuery('node');
