929b121901c75f985b2617578d1a58036d54a3b0
Rob Powell <rob.p.powell@gmail.com>
2017-08-10 14:08:14 -0400

    code sniffer clean up

--- docroot/modules/custom/mass_flagging/src/Controller/HelpPageController.php
+++ docroot/modules/custom/mass_flagging/src/Controller/HelpPageController.php
@@ -4,18 +4,23 @@ namespace Drupal\mass_flagging\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
+/**
+ * HelpPageController class.
+ */
 class HelpPageController extends ControllerBase {
 
   /**
    * Display the markup.
    *
    * @return array
+   *   Array to get rendered.
    */
   public function content() {
     module_load_include('inc', 'mass_flagging');
-    return array(
+    return [
       '#type' => 'markup',
       '#markup' => get_mass_flagging_help_text(),
-    );
+    ];
   }
+
 }
