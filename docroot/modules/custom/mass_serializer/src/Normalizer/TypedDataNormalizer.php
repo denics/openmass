ee4b949c097cb14584d597a3870bd24412ad38c9
Claudio Patrone <cpatrone@users.noreply.github.com>
2017-08-24 12:54:06 -0300

    MPRR-457: Add missing fields and reformat existent fields in Data.json Output (#1155)
    Ticket: Add missing fields and reformat existent fields in Data.json Output https://jira.state.ma.us/browse/MPRR-457 
    Ticket: Mass.Gov Form: Update to License Field https://jira.state.ma.us/browse/MPRR-489.
    * Updated license field on Document media entity
    * data.json valid 100% against https://labs.data.gov/dashboard/validate
    * Fixed @id value of feed to be dynamic

--- docroot/modules/custom/mass_serializer/src/Normalizer/TypedDataNormalizer.php
+++ docroot/modules/custom/mass_serializer/src/Normalizer/TypedDataNormalizer.php
@@ -2,27 +2,34 @@
 
 namespace Drupal\mass_serializer\Normalizer;
 
+use Drupal\Core\TypedData\TypedDataInterface;
 use Drupal\serialization\Normalizer\NormalizerBase;
 
 /**
  * Converts typed data objects to arrays.
  */
-class MassTypedDataNormalizer extends NormalizerBase {
+class TypedDataNormalizer extends NormalizerBase {
   /**
    * The interface or class that this Normalizer supports.
    *
    * @var string
    */
-  protected $supportedInterfaceOrClass = 'Drupal\Core\TypedData\TypedDataInterface';
+  protected $supportedInterfaceOrClass = TypedDataInterface::class;
 
   /**
    * {@inheritdoc}
    */
   public function normalize($object, $format = NULL, array $context = []) {
     $value = $object->getValue();
+
     if (isset($value[0]) && isset($value[0]['value'])) {
       $value = $value[0]['value'];
     }
+
+    if (is_array($value) && count($value) == 1) {
+      $value = current($value);
+    }
+
     return $value;
   }
 
